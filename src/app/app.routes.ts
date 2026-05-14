a69b7e35b17e42b6cbc3b710a4ec74a950915fbb
Jack Hoffsten <jack.hoffsten@hotmail.se>
2026-05-14 23:33:08 +0200

    Updates routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
+import { BookListComponent } from './books/book-list/book-list.component';
+import { BookFormComponent } from './books/book-form/book-form.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 export const routes: Routes = [
-  { path: 'login', loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent) },
-  { path: 'register', loadComponent: () => import('./auth/register/register.component').then(m => m.RegisterComponent) },
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'books', component: BookListComponent, canActivate: [authGuard] },
+  { path: 'books/new', component: BookFormComponent, canActivate: [authGuard] },
+  { path: 'books/:id', component: BookFormComponent, canActivate: [authGuard] },
+  { path: '**', redirectTo: 'books' },
 ];
