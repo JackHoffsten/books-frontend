1698bcf3471e382b4c3a1699c6ada989aec808bc
Jack Hoffsten <jack.hoffsten@hotmail.se>
2026-05-19 15:08:25 +0200

    Adds username to local storage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { LoginCredentials, RegisterCredentials, AuthResponse } from '../models/user.model';
+import { LoginCredentials, RegisterCredentials, AuthResponse, User } from '../models/user.model';
 import { Observable, tap } from 'rxjs';
 import { CookieService } from './cookie.service';
 import { ApiService } from './api.service';
@@ -15,17 +15,26 @@ export class AuthService {
 
   private readonly API_URL = this.apiService.getUrl('/auth');
   private readonly ACCESS_TOKEN_KEY = 'access_token';
+  private readonly USERNAME_KEY = 'username';
   private readonly REFRESH_TOKEN_KEY = 'refresh_token';
+
+  public currentUser: User | null = null;
 
   login(credentials: LoginCredentials): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${this.API_URL}/login`, credentials).pipe(
-      tap((response) => this.setAccessToken(response.accessToken))
+      tap((response) => {
+        this.setAccessToken(response.accessToken);
+        this.setUser(response.username, response.email);
+      })
     );
   }
 
   register(credentials: RegisterCredentials): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${this.API_URL}/register`, credentials).pipe(
-      tap(response => this.setAccessToken(response.accessToken))
+      tap((response) => {
+        this.setAccessToken(response.accessToken);
+        this.setUser(response.username, response.email);
+      })
     );
   }
 
@@ -39,8 +48,17 @@ export class AuthService {
     localStorage.setItem(this.ACCESS_TOKEN_KEY, accessToken);
   }
 
+  private setUser(username: string, email: string) {
+    this.currentUser = { username: username, email: email };
+    localStorage.setItem(this.USERNAME_KEY, username);
+  }
+
   getAccessToken(): string | null {
     return localStorage.getItem(this.ACCESS_TOKEN_KEY);
+  }
+
+  getUsername(): string | null {
+    return localStorage.getItem(this.USERNAME_KEY);
   }
 
   getRefreshToken(): string | null {
@@ -53,6 +71,8 @@ export class AuthService {
 
   logout() {
     localStorage.removeItem(this.ACCESS_TOKEN_KEY);
+    localStorage.removeItem(this.USERNAME_KEY);
     this.cookieService.deleteCookie(this.REFRESH_TOKEN_KEY);
+    this.currentUser = null;
   }
 }
